a5a6d22074dd03ab7f121005989c6e2021608eb6
missfo <80922974+missfo@users.noreply.github.com>
2021-05-02 23:25:54 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const myHeading = document.getElementById("headingH1");
+myHeading.textContent = "Welcome to My World!";
+
+function searchFunction() {
+    const searchTerm = document.getElementById("searchInput");
+    console.log("We are searching for: " + searchTerm.value);
+}
